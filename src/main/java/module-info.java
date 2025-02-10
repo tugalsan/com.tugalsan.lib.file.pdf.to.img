cb1c4525c82edf8d8918e94db7f74f9fa8f326a5
tugalsan karabacak <tugalsan@users.noreply.github.com>
2025-02-10 11:48:04 +0300

    .

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,7 @@ module com.tugalsan.lib.file.pdf.to.img {
     requires com.tugalsan.api.os;
     requires com.tugalsan.api.file;
     requires com.tugalsan.api.file.properties;
-    requires com.tugalsan.api.unsafe;
+    
     requires com.tugalsan.api.union;
     requires com.tugalsan.api.function;
     requires com.tugalsan.api.log;
